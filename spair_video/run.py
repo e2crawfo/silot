c899e0dbe7afa0d500916ece3872164ad64f37ad
Eric Crawford <eric.crawford@mail.mcgill.ca>
2019-06-26 13:10:11 -0400

    Small change to config.

--- spair_video/run.py
+++ spair_video/run.py
@@ -16,7 +16,7 @@ from spair_video.core import SimpleVideoVAE, SimpleVAE_RenderHook, BackgroundExt
 from spair_video.tba import TrackingByAnimation, TBA_Backbone, TBA_RenderHook
 from spair_video.seq_air import SQAIR, SQAIRUpdater, SQAIR_RenderHook
 from spair_video.sspair import SequentialSpair, SequentialSpair_RenderHook
-from spair_video.interpretable_sspair import SILOT, SILOT_RenderHook
+from spair_video.silot import SILOT, SILOT_RenderHook
 from spair_video.background_only import BackgroundOnly, BackgroundOnly_RenderHook
 from spair_video.baseline import BaselineTracker, Baseline_RenderHook
 
@@ -267,120 +267,123 @@ def spair_prepare_func():
     cfg.training_wheels = "Exp(1.0, 0.0, decay_rate=0.0, decay_steps={}, staircase=True)".format(cfg.end_training_wheels)
 
 
-alg_configs = dict(
-    simple_vae=Config(
-        build_network=SimpleVideoVAE,
+alg_configs = dict()
 
-        attr_prior_mean=0.,
-        attr_prior_std=1.0,
+alg_configs['simple_vae'] = Config(
+    build_network=SimpleVideoVAE,
 
-        A=128,
+    attr_prior_mean=0.,
+    attr_prior_std=1.0,
 
-        train_reconstruction=True,
-        reconstruction_weight=1.0,
+    A=128,
 
-        train_kl=True,
-        kl_weight=1.0,
+    train_reconstruction=True,
+    reconstruction_weight=1.0,
 
-        build_encoder=lambda scope: MLP(n_units=[128, 128, 128], scope=scope),
-        build_decoder=lambda scope: MLP(n_units=[128, 128, 128], scope=scope),
-        build_cell=lambda scope: CompositeCell(
-            tf.contrib.rnn.GRUBlockCellV2(128),
-            MLP(n_units=[128], scope="GRU"), 2*128),
-        render_hook=SimpleVAE_RenderHook(),
+    train_kl=True,
+    kl_weight=1.0,
+
+    build_encoder=lambda scope: MLP(n_units=[128, 128, 128], scope=scope),
+    build_decoder=lambda scope: MLP(n_units=[128, 128, 128], scope=scope),
+    build_cell=lambda scope: CompositeCell(
+        tf.contrib.rnn.GRUBlockCellV2(128),
+        MLP(n_units=[128], scope="GRU"), 2*128),
+    render_hook=SimpleVAE_RenderHook(),
+)
+
+# --- SSPAIR --- (just applying SPAIR separately to each frame)
+
+alg_configs['sspair'] = Config(
+    build_network=SequentialSpair,
+    render_hook=SequentialSpair_RenderHook(),
+    prepare_func=spair_prepare_func,
+    n_objects_per_cell=1,
+
+    # stopping_criteria="MOT:mota,max",
+    # stopping_criteria="AP,max",
+    # threshold=np.inf,
+
+    RecurrentGridConvNet=dict(
+        bidirectional=False,
+        # build_cell=lambda n_hidden, scope: CompositeCell(
+        #     tf.contrib.rnn.GRUBlockCellV2(n_hidden),
+        #     MLP(n_units=[n_hidden], scope="GRU"), n_hidden),
+        build_cell=None,
     ),
-    sspair=Config(
-        build_network=SequentialSpair,
-        render_hook=SequentialSpair_RenderHook(),
-        prepare_func=spair_prepare_func,
-        n_objects_per_cell=1,
-
-        # stopping_criteria="MOT:mota,max",
-        # stopping_criteria="AP,max",
-        # threshold=np.inf,
-
-        RecurrentGridConvNet=dict(
-            bidirectional=False,
-            # build_cell=lambda n_hidden, scope: CompositeCell(
-            #     tf.contrib.rnn.GRUBlockCellV2(n_hidden),
-            #     MLP(n_units=[n_hidden], scope="GRU"), n_hidden),
-            build_cell=None,
-        ),
-        build_backbone=lambda scope: RecurrentGridConvNet(
-            layers=[
-                dict(filters=128, kernel_size=4, strides=3),
-                dict(filters=128, kernel_size=4, strides=2),
-                dict(filters=128, kernel_size=4, strides=2),
-                dict(filters=128, kernel_size=1, strides=1),
-                dict(filters=128, kernel_size=1, strides=1),
-                dict(filters=128, kernel_size=1, strides=1),
-            ],
-            scope=scope,
-        ),
-        build_feature_fuser=lambda scope: ConvNet(
-            scope=scope, layers=[
-                dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
-                dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
-            ],
-        ),
-        build_obj_feature_extractor=lambda scope: ConvNet(
-            scope=scope, layers=[
-                dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
-                dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
-            ],
-        ),
-
-        build_lateral=lambda scope: MLP(n_units=[100, 100], scope=scope),
-        build_object_encoder=lambda scope: MLP(n_units=[256, 128], scope=scope),
-        build_object_decoder=lambda scope: MLP(n_units=[128, 256], scope=scope),
-
-        n_backbone_features=64,
-        n_passthrough_features=64,
-
-        n_lookback=1,
-
-        use_concrete_kl=False,
-        obj_concrete_temp=1.0,
-        obj_temp=1.0,
-        importance_temp=.25,
-        object_shape=(14, 14),
-        A=64,
-
-        min_hw=0.0,
-        max_hw=1.0,
-
-        min_yx=-0.5,
-        max_yx=1.5,
-
-        yx_prior_mean=0.0,
-        yx_prior_std=1.0,
-
-        attr_prior_mean=0.0,
-        attr_prior_std=1.0,
-        z_prior_mean=0.0,
-        z_prior_std=1.0,
-
-        color_logit_scale=2.0,
-
-        # values we were using previously, resulted in large bounding boxes
-        # alpha_logit_scale=1.0,
-        # alpha_logit_bias=0.0,
-
-        # values used in SPAIR
-        alpha_logit_scale=0.1,
-        alpha_logit_bias=5.0,
-
-        end_training_wheels=1000,
-        count_prior_dist=None,
-        noise_schedule="Exp(0.001, 0.0, 1000, 0.1)",
-
-        # Found through hyper parameter search
-        hw_prior_mean=float(np.log(0.1/0.9)),
-        hw_prior_std=0.5,
-        count_prior_decay_steps=1000,
-        initial_count_prior_log_odds=1e6,
-        final_count_prior_log_odds=0.0125,
+    build_backbone=lambda scope: RecurrentGridConvNet(
+        layers=[
+            dict(filters=128, kernel_size=4, strides=3),
+            dict(filters=128, kernel_size=4, strides=2),
+            dict(filters=128, kernel_size=4, strides=2),
+            dict(filters=128, kernel_size=1, strides=1),
+            dict(filters=128, kernel_size=1, strides=1),
+            dict(filters=128, kernel_size=1, strides=1),
+        ],
+        scope=scope,
     ),
+    build_feature_fuser=lambda scope: ConvNet(
+        scope=scope, layers=[
+            dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
+            dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
+        ],
+    ),
+    build_obj_feature_extractor=lambda scope: ConvNet(
+        scope=scope, layers=[
+            dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
+            dict(filters=None, kernel_size=3, strides=1, padding="SAME"),
+        ],
+    ),
+
+    build_lateral=lambda scope: MLP(n_units=[100, 100], scope=scope),
+    build_object_encoder=lambda scope: MLP(n_units=[256, 128], scope=scope),
+    build_object_decoder=lambda scope: MLP(n_units=[128, 256], scope=scope),
+
+    n_backbone_features=64,
+    n_passthrough_features=64,
+
+    n_lookback=1,
+
+    use_concrete_kl=False,
+    obj_concrete_temp=1.0,
+    obj_temp=1.0,
+    importance_temp=.25,
+    object_shape=(14, 14),
+    A=64,
+
+    min_hw=0.0,
+    max_hw=1.0,
+
+    min_yx=-0.5,
+    max_yx=1.5,
+
+    yx_prior_mean=0.0,
+    yx_prior_std=1.0,
+
+    attr_prior_mean=0.0,
+    attr_prior_std=1.0,
+    z_prior_mean=0.0,
+    z_prior_std=1.0,
+
+    color_logit_scale=2.0,
+
+    # values we were using previously, resulted in large bounding boxes
+    # alpha_logit_scale=1.0,
+    # alpha_logit_bias=0.0,
+
+    # values used in SPAIR
+    alpha_logit_scale=0.1,
+    alpha_logit_bias=5.0,
+
+    end_training_wheels=1000,
+    count_prior_dist=None,
+    noise_schedule="Exp(0.001, 0.0, 1000, 0.1)",
+
+    # Found through hyper parameter search
+    hw_prior_mean=float(np.log(0.1/0.9)),
+    hw_prior_std=0.5,
+    count_prior_decay_steps=1000,
+    initial_count_prior_log_odds=1e6,
+    final_count_prior_log_odds=0.0125,
 )
 
 alg_configs["indep_sspair"] = alg_configs["sspair"].copy(
