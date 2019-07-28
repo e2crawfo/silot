0c42aa89bd5b2c3464695b8512841dc303d8a969
Eric Crawford <eric.crawford@mail.mcgill.ca>
2019-07-27 21:03:33 -0400

    Updates to experiments.

--- spair_video/experiments/shapes/run.sh
+++ spair_video/experiments/shapes/run.sh
@@ -1,8 +1,12 @@
-# python silot_run.py long --max-shapes=10
-# python silot_run.py long --small --max-shapes=10
-# python silot_run.py long --max-shapes=20
-# python silot_run.py long --small --max-shapes=20
+python silot_run.py long --max-shapes=10
+python silot_run.py long --small --max-shapes=10
+python silot_run.py long --max-shapes=20
+python silot_run.py long --small --max-shapes=20
 python silot_run.py long --max-shapes=30
 python silot_run.py long --small --max-shapes=30
+# python silot_run.py short --max-shapes=10
+# python silot_run.py short --small --max-shapes=10
+# python silot_run.py short --max-shapes=20
+# python silot_run.py short --small --max-shapes=20
 # python silot_run.py short --max-shapes=30
 # python silot_run.py short --small --max-shapes=30
--- spair_video/experiments/shapes/silot_run.py
+++ spair_video/experiments/shapes/silot_run.py
@@ -13,17 +13,18 @@ args, _ = parser.parse_known_args()
 readme = "Running SILOT experiment on shapes."
 
 run_kwargs = dict(
-    max_hosts=1, ppn=6, cpp=2, gpu_set="0,1", pmem=10000, project="rpp-bengioy",
-    wall_time="96hours", cleanup_time="5mins", slack_time="5mins", n_repeats=6,
+    max_hosts=1, ppn=4, cpp=2, gpu_set="0,1", pmem=15000, project="rpp-bengioy",
+    wall_time="96hours", cleanup_time="5mins", slack_time="5mins", n_repeats=4,
     copy_locally=True,
 )
 
 durations = dict(
     long=copy_update(run_kwargs),
-    single=copy_update(run_kwargs, gpu_set="0", ppn=1, n_repeats=1),
     short=dict(
-        wall_time="180mins", gpu_set="0", ppn=3, n_repeats=3, distributions=None,
-        config=dict(max_steps=3000, render_step=500, eval_step=100, display_step=100, stage_steps=600, curriculum=[dict()]),
+        wall_time="60mins", gpu_set="0", ppn=2, n_repeats=2, distributions=None,
+        config=dict(
+            max_steps=3000, render_step=500, eval_step=500, display_step=100,
+            stage_steps=600, curriculum=[dict()]),
     ),
     build=dict(
         ppn=1, cpp=3, gpu_set="0", wall_time="300mins", n_repeats=1, distributions=None,
@@ -37,7 +38,7 @@ durations = dict(
 config = basic_config.copy()
 if args.small:
     config.update(env_configs['big_shapes_small'])
-    config.n_prop_objects = 8
+    config.n_prop_objects = 10
 else:
     config.update(env_configs['big_shapes'])
     config.n_prop_objects = 30
@@ -45,7 +46,28 @@ else:
 config.update(alg_configs['shapes_silot'])
 config.update(
     min_shapes=args.max_shapes-9, max_shapes=args.max_shapes,
-    stage_steps=40000, render_step=1000000)
+    stage_steps=40000, render_step=1000000,
+    final_count_prior_log_odds=2.5,
+    curriculum=[
+        dict(),
+        dict(
+            patience_start=1,
+            lr_schedule=1. / 3 * 1e-4,
+            initial_n_frames=8,
+            initial_count_prior_log_odds=2.5,
+            end_training_wheels=1,
+            noise_schedule=0.0,
+        ),
+        dict(
+            patience_start=1,
+            lr_schedule=1. / 9 * 1e-4,
+            initial_n_frames=8,
+            initial_count_prior_log_odds=2.5,
+            end_training_wheels=1,
+            noise_schedule=0.0,
+        ),
+    ],
+)
 
 run_experiment(
     "shapes_silot", config, "silot on shapes.", name_variables="max_shapes", durations=durations
